e44c9007da8566daa427cf2015273878dc07a2f9
santosh-b <43103834+santosh-b@users.noreply.github.com>
2021-01-31 21:09:50 -0600

    Update train_fgsm.py

--- fast_adversarial-master/CIFAR10/train_fgsm.py
+++ fast_adversarial-master/CIFAR10/train_fgsm.py
@@ -18,11 +18,12 @@ from utils import (upper_limit, lower_limit, std, clamp, get_loaders,
     attack_pgd, evaluate_pgd, evaluate_standard)
 
 logger = logging.getLogger(__name__)
-
+import sys
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model-dir', default='', type=str)
+    parser.add_argument('--model', default='vgg', type=str)
     parser.add_argument('--cfg-dir', default='', type=str)
     parser.add_argument('--batch-size', default=128, type=int)
     parser.add_argument('--data-dir', default='../../cifar-data', type=str)
@@ -31,6 +32,7 @@ def get_args():
     parser.add_argument('--lr-schedule', default='cyclic', choices=['cyclic', 'multistep'])
     parser.add_argument('--lr-min', default=0., type=float)
     parser.add_argument('--lr-max', default=0.2, type=float)
+    parser.add_argument('--cfg', default=None, type=str)
     parser.add_argument('--weight-decay', default=5e-4, type=float)
     parser.add_argument('--momentum', default=0.9, type=float)
     parser.add_argument('--epsilon', default=8, type=int)
@@ -55,6 +57,201 @@ defaultcfg = {
     16 : [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512],
     19 : [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512],
 }
+
+class channel_selection(nn.Module):
+    """
+    Select channels from the output of BatchNorm2d layer. It should be put directly after BatchNorm2d layer.
+    The output shape of this layer is determined by the number of 1 in `self.indexes`.
+    """
+    def __init__(self, num_channels):
+        """
+        Initialize the `indexes` with all one vector with the length same as the number of channels.
+        During pruning, the places in `indexes` which correpond to the channels to be pruned will be set to 0.
+	    """
+        super(channel_selection, self).__init__()
+        self.indexes = nn.Parameter(torch.ones(num_channels))
+
+    def forward(self, input_tensor):
+        """
+        Parameter
+        ---------
+        input_tensor: (N,C,H,W). It should be the output of BatchNorm2d layer.
+		"""
+        selected_index = np.squeeze(np.argwhere(self.indexes.data.cpu().numpy()))
+        if selected_index.size == 1:
+            selected_index = np.resize(selected_index, (1,)) 
+        output = input_tensor[:, selected_index, :, :]
+        return output
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, cfg, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.select = channel_selection(inplanes)
+        self.conv1 = conv3x3(cfg[0], cfg[1], stride)
+        self.bn1 = nn.BatchNorm2d(cfg[1])
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(cfg[1], planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.select(x)
+        out = self.conv1(out)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, seed, num_classes=10, cfg=None):
+        super(ResNet, self).__init__()
+        self.inplanes = 64
+        rng = torch.manual_seed(seed)
+        if cfg is None:
+            # Construct config variable (basic block)
+            cfg = [[64], [64, 64]*layers[0], [128, 128]*layers[1], [256, 256]*layers[2], [512, 512]* layers[3]]
+            cfg = [item for sub_list in cfg for item in sub_list]
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 64, layers[0], cfg=cfg[0:2*layers[0]])
+        self.layer2 = self._make_layer(block, 128, layers[1], cfg=cfg[2*layers[0]: 2*(layers[0]+layers[1])], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], cfg=cfg[2*(layers[0]+layers[1]): 2*(layers[0]+layers[1]+layers[2])], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], cfg=cfg[2*(layers[0]+layers[1]+layers[2]): 2*(layers[0]+layers[1]+layers[2]+layers[3])], stride=2)
+        self.avgpool = nn.AvgPool2d(4, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.select = channel_selection(64 * block.expansion)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n), generator=rng)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(0.5)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01, generator=rng)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, cfg, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1,
+                          stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, cfg[0:2], stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, cfg[2*i: 2*(i+1)]))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x, with_latent=False, fake_relu=False, no_relu=False):
+        assert (not fake_relu) and (not no_relu),  \
+            "fake_relu and no_relu not yet supported for this architecture"
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.select(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        latent = x.view(x.size(0), -1)
+        y = self.fc(latent)
+        if with_latent:
+            return y, latent
+        return y
+
+
+def resnet18(pretrained=False, cfg=None, num_classes=10, seed=0, **kwargs):
+    """Constructs a ResNet-18 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], seed, num_classes=num_classes, cfg=cfg, **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet18'])))
+    return model
+
+
+def resnet34(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet34'])))
+    return model
+
+
+def resnet50(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet50'])))
+    return model
+
+
+def resnet101(pretrained=False, **kwargs):
+    """Constructs a ResNet-101 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet101'])))
+    return model
+
+
+def resnet152(pretrained=False, **kwargs):
+    """Constructs a ResNet-152 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet152'])))
+    return model
 
 class vgg(nn.Module):
     def __init__(self, depth, dataset='cifar10', init_weights=True, cfg=None, seed=0):
@@ -142,14 +339,25 @@ def main():
     epsilon = (args.epsilon / 255.) / std(args.dataset)
     alpha = (args.alpha / 255.) / std(args.dataset)
     pgd_alpha = (2 / 255.) / std(args.dataset)
-
-    pruned_cfg = torch.load(args.cfg_dir)['cfg']
-    if args.dataset == 'cifar10':
-        train_loader, test_loader = get_loaders(args.data_dir, args.batch_size, 'cifar10')
-        model = vgg(16, cfg=pruned_cfg, seed=0)
-    elif args.dataset == 'cifar100':
-        train_loader, test_loader = get_loaders(args.data_dir, args.batch_size, 'cifar100')
-        model = vgg(16, cfg=pruned_cfg, dataset='cifar100', seed=0)
+    if args.cfg is not None:
+        pruned_cfg = [int(x) for x in args.cfg.replace('[','').replace(']','').replace(' ','').split(',') if x.isnumeric()]
+        print(pruned_cfg)
+    else:
+        pruned_cfg = torch.load(args.cfg_dir)['cfg']
+    if args.model == 'vgg':
+        if args.dataset == 'cifar10':
+            train_loader, test_loader = get_loaders(args.data_dir, args.batch_size, 'cifar10')
+            model = vgg(16, cfg=pruned_cfg, seed=0)
+        elif args.dataset == 'cifar100':
+            train_loader, test_loader = get_loaders(args.data_dir, args.batch_size, 'cifar100')
+            model = vgg(16, cfg=pruned_cfg, dataset='cifar100', seed=0)
+    if args.model == 'resnet':
+        if args.dataset == 'cifar10':
+            train_loader, test_loader = get_loaders(args.data_dir, args.batch_size, 'cifar10')
+            model = resnet18(seed=0, cfg=pruned_cfg, num_classes=10)
+        elif args.dataset == 'cifar100':
+            train_loader, test_loader = get_loaders(args.data_dir, args.batch_size, 'cifar100')
+            model = resnet18(seed=0, cfg=pruned_cfg, num_classes=100)
     model.load_state_dict(torch.load(args.model_dir))
     model.cuda()
     model.train()
@@ -213,7 +421,7 @@ def main():
         if args.early_stop:
             # Check current PGD robustness of model using random minibatch
             X, y = first_batch
-            pgd_delta = attack_pgd(model, X, y, epsilon, pgd_alpha, 5, 1, opt)
+            pgd_delta = attack_pgd(model, X, y, epsilon, pgd_alpha, 5, 1, opt, args.dataset)
             with torch.no_grad():
                 output = model(clamp(X + pgd_delta[:X.size(0)], lower_limit(args.dataset), upper_limit(args.dataset)))
             robust_acc = (output.max(1)[1] == y).sum().item() / y.size(0)
@@ -223,7 +431,7 @@ def main():
             best_state_dict = copy.deepcopy(model.state_dict())
         epoch_time = time.time()
         lr = scheduler.get_lr()[0]
-        pgd_loss, pgd_acc = evaluate_pgd(test_loader, model, 7, 5)
+        pgd_loss, pgd_acc = evaluate_pgd(test_loader, model, 7, 5, args.dataset)
         test_loss, test_acc = evaluate_standard(test_loader, model)
         logger.info(f'epoch {epoch}: {pgd_acc} {test_acc} {pgd_loss} {test_loss}')
         logger.info('%d \t %.1f \t \t %.4f \t %.4f \t %.4f',
