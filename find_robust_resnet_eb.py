ec9ad877ecb1ec0029bb1e8835b0a803620952d8
santosh-b <43103834+santosh-b@users.noreply.github.com>
2021-02-07 15:07:42 -0600

    Update find_robust_resnet_eb.py

--- find_robust_resnet_eb.py
+++ find_robust_resnet_eb.py
@@ -33,6 +33,7 @@ train_args = Parameters({
     'save_ckpt_iters': 110,
     'weight_decay':5e-4,
     'data_aug': 0,
+    'lr': 1,
     'log_iters': 1,
     'mixed_precision':0,
     'custom_lr_multiplier': '[(0,.1),(100,.01),(105,.001)]'
